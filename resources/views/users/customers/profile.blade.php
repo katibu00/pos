339ac043ccdb8fd7d0beaefa315b8ad54045940a
katibu01 <ukmisau@gmail.com>
2023-08-13 14:55:24 +0100

    update

--- resources/views/users/customers/profile.blade.php
+++ resources/views/users/customers/profile.blade.php
@@ -80,7 +80,7 @@
                                                                 ->get(); 
                                                             $returns = App\Models\Returns::select('product_id', 'price', 'quantity', 'discount', 'payment_method')
                                                                 ->where('return_no', 'R'.$date->receipt_no)
-                                                                ->where('customer_name', $user->id)
+                                                                ->where('customer', $user->id)
                                                                 ->get();
                                                         @endphp
                                                         <tr>
@@ -403,6 +403,7 @@
 
                                         $returns = App\Models\Returns::select('product_id', 'price', 'quantity', 'discount', 'payment_method')
                                                 ->where('return_no', 'R'.$date->receipt_no)
+                                                ->where('customer', $user->id)
                                                 ->get();
                                             
                                         foreach ($sales as $sale) {

bccbb8f7f5a0638b3bab88b9e7202dc93bbc9902
katibu01 <ukmisau@gmail.com>
2024-11-03 14:17:35 +0100

    add R

--- resources/views/users/customers/profile.blade.php
+++ resources/views/users/customers/profile.blade.php
@@ -100,7 +100,7 @@
                                                                     'discount',
                                                                     'payment_method',
                                                                 )
-                                                                    ->where('return_no', $date->receipt_no)
+                                                                    ->where('return_no', 'R'.$date->receipt_no)
                                                                     ->get();
                                                             @endphp
                                                             <tr>
@@ -529,7 +529,7 @@
                                                 'discount',
                                                 'payment_method',
                                             )
-                                                ->where('return_no', $date->receipt_no)
+                                                ->where('return_no','R'. $date->receipt_no)
                                                 ->get();
 
                                             foreach ($sales as $sale) {
